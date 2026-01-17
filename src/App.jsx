4ef0e0798eb6e9c1d33a9dbe508c683a5419a6f3
Ashwini955 <ashwinirm052@gmail.com>
2026-01-17 16:11:49 +0530

    change lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About/About"
 import Home from "./components/Home/Home"
-import Nav from "./components/nav/nav"
+import Nav from "./components/Nav/Nav"
 import Project from "./components/Project/Project"
 import Contact from "./components/Contact/Contact"
 
